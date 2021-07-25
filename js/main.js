ae8d60ab0b00f6ebc2200b03aa5efe107ad94110
Amanda <amanda.smith590@yahoo.com>
2021-07-25 16:11:18 -0700

    added footer, login modal opens and closes

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ const modal = document.querySelector('.modal')
 const loginBtn = document.querySelector('.login-btn')
 const closeBtn = document.querySelector('.close')
 
+
 loginBtn.addEventListener('click', openModal)
 closeBtn.addEventListener('click', closeModal)
 window.addEventListener('click', outsideClick)
+
 
 function openModal() {
   modal.style.display = 'block'
@@ -19,3 +21,26 @@ function outsideClick(e) {
     closeModal()
   }
 }
+
+
+// const modal = document.querySelector('.modal')
+// const loginBtn = document.querySelector('.login-btn')
+// const closeBtn = document.querySelector('.close')
+
+// loginBtn.addEventListener('click', openModal)
+// closeBtn.addEventListener('click', closeModal)
+// window.addEventListener('click', outsideClick)
+
+// function openModal() {
+//   modal.style.display = 'block'
+// }
+
+// function closeModal() {
+//   modal.style.display = 'none'
+// }
+
+// function outsideClick(e) {
+//   if (e.target == modal) {
+//     closeModal()
+//   }
+// }
